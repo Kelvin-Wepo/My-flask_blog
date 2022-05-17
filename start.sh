79e7a110b1f49a1d2ce30a96d170a5e4d92875c5
Kelvin wepo <kelvin.wepo@student.moringaschool.com>
2022-05-17 21:18:52 +0300

    final commit

---
+++ start.sh
@@ -0,0 +1,9 @@
+export SECRET_KEY='Kwepo1234'
+# export APP_SETTINGS="config.DevelopmentConfig"
+
+#  export DATABASE_URL='postgresql+psycopg2://moringa:access@localhost/blog'
+
+python3 manage.py server
+# python3 manage.py db migrate -m "redo migrations"
+# python manage.py db migrate -m "Deployment"
+# python manage.py db upgrade
